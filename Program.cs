dec307b50803a1d2f6eaff400cbce41dd258dad8
Richard Clarke <113057777+MrTechnol0gy@users.noreply.github.com>
2022-10-11 09:59:12 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,8 +8,109 @@ namespace Conditionals
 {
     internal class Program
     {
+        static int health;
+        static int enemydamage;
+        static bool loopend;
+        static int weapon;
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Conditionals.");
+            Console.WriteLine("------------------");
+            Console.WriteLine();
+
+            health = 100;
+            enemydamage = 10;
+            weapon = 0;
+            loopend = false;
+
+            while (loopend == false)
+            {
+            TakeDamage(enemydamage);
+            ShowHud();
+            
+            }
+            
+            Console.ReadKey(true);
+        }
+
+        static void TakeDamage(int damage)
+        {
+            Console.WriteLine("Player is about to take " + damage + " damage...");
+            health = health - damage;
+
+            if (health < 0)
+            {
+                health = 0;
+            }
+        }
+
+        static void HealthCheck(int health)
+        {
+            if (health > 75)
+            {
+                Console.WriteLine("Healthy");
+            }
+            else if (health > 49 && health < 75)
+            {
+                Console.WriteLine("Injured");
+            }
+            else if (health > 24 && health < 50)
+            {
+                Console.WriteLine("Badly Injured");
+            }
+            else if (health > 0 && health < 25)
+            {
+                Console.WriteLine("Death Imminent");
+            }
+        }
+
+        static void ShowHud()
+        {
+            Console.WriteLine("Health: " + health);
+            HealthCheck(health);
+            WeaponCheck(weapon);            
+            DeathCheck(health);
+            Console.WriteLine();
+        }
+
+        static void DeathCheck(int health)
+        {
+            if (health <= 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("You are deceased. Your loved ones, if any, will be notified.");
+                loopend = true;
+            }
+        }
+
+        static void WeaponCheck(int currentweapon)
+        {
+            if (currentweapon == 0)
+            {
+                Console.WriteLine("Currently Equipped Weapon: Fists");
+                weapon++;
+            }
+            else if (currentweapon == 1)
+            {
+                Console.WriteLine("Currently Equipped Weapon: Brass Knuckles");
+                weapon++;
+            }
+            else if (currentweapon == 2)
+            {
+                Console.WriteLine("Currently Equipped Weapon: Spiked Knuckles");
+                weapon++;
+            }
+            else if (currentweapon == 3)
+            {
+                Console.WriteLine("Currently Equipped Weapon: Flaming Knuckles");
+                weapon++;
+            }
+            else
+            {
+                Console.WriteLine("Currently Equipped Weapon: Infinite Knuckles");
+                weapon = 0;
+            }            
         }
     }
 }
